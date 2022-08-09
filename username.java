90c69a212dafa5aee28fec0ba259f72955bb9d59
soumayadip2002 <sinuroy696@gmail.com>
2022-08-09 21:04:43 +0530

    first commit

---
+++ username.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class UsernameValidator {
+    
+    
+    public static final String regularExpression = null;
+}
+
+
+public class username {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
