cbe1bae23ba470504494f82c1d5c44152fbb9e5d
Soumayadip Saha <106472567+soumayadip2002@users.noreply.github.com>
2022-10-12 21:18:09 +0530

    Update Candel.java

--- Candel.java
+++ Candel.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Candel {
+public class CandelCount {
     public static void main(String[] x){
         try (Scanner sc = new Scanner(System.in)) {
             int num = sc.nextInt();
